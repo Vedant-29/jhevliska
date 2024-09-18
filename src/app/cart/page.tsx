e84fbfaf751f4c5408564f5e5c71796d406269bd
Vedant-29 <agarwalvedant42@gmail.com>
2024-09-18 17:15:22 +0530

    Editing form --3

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,6 +1,6 @@
 import { useCart } from '@/context/cartContext';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { AiOutlineClose } from 'react-icons/ai';
 
 
