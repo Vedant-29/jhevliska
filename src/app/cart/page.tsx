519ac21002f0c6a4d82cd2466c033283c6e8333f
Vedant-29 <agarwalvedant42@gmail.com>
2024-09-18 17:28:20 +0530

    Cart page bugs --2

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useCart } from '@/context/cartContext';
-import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { AiOutlineClose } from 'react-icons/ai';
 
